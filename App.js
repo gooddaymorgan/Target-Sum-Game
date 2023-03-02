294e1e6bb7c5dd4e2f55b62a0966002ed47f29dd
Morgan <morgan.em.day@gmail.com>
2023-03-02 16:56:18 -0600

    Pink Background & Text
    - added styles to the  component
    - added pink background color

--- App.js
+++ App.js
@@ -5,15 +5,22 @@
  */
 
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 class App extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Target Sum...</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    flex: 1,
+  },
+});
 export default App;
