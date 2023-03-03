91a7d90ff0202728503386c2068c77f08b632ddd
Morgan <morgan.em.day@gmail.com>
2023-03-02 20:24:55 -0600

    Game.js file added
    - file format fixed in eslint
    - is a copy / paste from old  App.js file
    - will be fixed / update this is placeholder <3

---
+++ Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.target}>42</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'pink',
+    flex: 1,
+  },
+});
+export default Game;
