92a12310ef5264229551194dfbecd0ce68cad87d
Morgan <morgan.em.day@gmail.com>
2023-03-04 00:54:11 -0600

    I'm just really tired I just really wanna go to bed. I did the work and learned a lot I'm just so tired and I haven't seen my partner in a week. Please
    App.js
    
    - added state component allows game to be reset. Everytime game reseted it adds to the counter on how many times the game has been played
    - game component added generates 6 differenrandom number that add up to our target sum
    - added a timer of 10 seconds
    - has an even that triggers the reset
    
    Game.js
    
    -imported button from reactive native
    -added seconds timer proptype
    -added play againprototypee
    -added state component that activates 'game status'
    -added shuffle numbers component that shuffles our randomly generated numbers so we have a challenge

--- App.js
+++ App.js
@@ -8,8 +8,23 @@ import React from 'react';
 import Game from './Game';
 
 class App extends React.Component {
+  state = {
+    gameId: 1,
+  };
+  resetGame = () => {
+    this.setState(prevState => {
+      return {gameId: prevState.gameId + 1};
+    });
+  };
   render() {
-    return <Game randomNumberCount={6} />;
+    return (
+      <Game
+        key={this.state.gameId}
+        onPlayAgain={this.resetGame}
+        randomNumberCount={6}
+        initialSeconds={10}
+      />
+    );
   }
 }
 
